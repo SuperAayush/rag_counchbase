b1a37f5d66ac351aacc8d47988c4b8fc24f7f5e1
Aayush Sharma <aayusharma1401@gmail.com>
2024-12-17 13:40:32 +0530

    feat: connect counchbase vector store and google embeddings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import streamlit as st
+from langchain_couchbase import CouchbaseVectorStore
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 @st.cache_resource(show_spinner="Connecting to Couchbase")
 def connect_to_couchbase(connection_string, db_username, db_password):
@@ -17,3 +19,74 @@ def connect_to_couchbase(connection_string, db_username, db_password):
     cluster.wait_until_ready(timedelta(seconds=5))
 
     return cluster
+
+@st.cache_resource(show_spinner="Connecting to Vector Store")
+def get_vector_store(
+    _cluster,
+    db_bucket,
+    db_scope,
+    db_collection,
+    _embedding,
+    index_name,
+):
+    """Return the Couchbase vector store"""
+    vector_store = CouchbaseVectorStore(
+        cluster=_cluster,
+        bucket_name=db_bucket,
+        scope_name=db_scope,
+        collection_name=db_collection,
+        embedding=_embedding,
+        index_name=index_name,
+    )
+    return vector_store
+
+def check_environment_variable(variable_name):
+    """Check if environment variable is set"""
+    if variable_name not in os.environ:
+        st.error(
+            f"{variable_name} environment variable is not set. Please add it to the secrets.toml file"
+        )
+        st.stop()
+
+if __name__ == "__main__":
+
+    st.set_page_config(
+        page_title="Chat with any PDF uploaded using Couchbase, Langchain & Gemini Pro",
+        layout="centered",
+        initial_sidebar_state="auto",
+        menu_items=None,
+    )
+
+    DB_CONN_STR = os.getenv("DB_CONN_STR")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_BUCKET = os.getenv("DB_BUCKET")
+    DB_SCOPE = os.getenv("DB_SCOPE")
+    DB_COLLECTION = os.getenv("DB_COLLECTION")
+    INDEX_NAME = os.getenv("INDEX_NAME")
+
+    check_environment_variable("GOOGLE_API_KEY")
+    check_environment_variable("DB_CONN_STR")
+    check_environment_variable("DB_USERNAME")
+    check_environment_variable("DB_PASSWORD")
+    check_environment_variable("DB_BUCKET")
+    check_environment_variable("DB_SCOPE")
+    check_environment_variable("DB_COLLECTION")
+    check_environment_variable("INDEX_NAME")
+
+    embedding = GoogleGenerativeAIEmbeddings(
+        model="models/text-embedding-004",
+    )
+
+    cluster = connect_to_couchbase(DB_CONN_STR, DB_USERNAME, DB_PASSWORD)
+
+    vector_store = get_vector_store(
+        cluster,
+        DB_BUCKET,
+        DB_SCOPE,
+        DB_COLLECTION,
+        embedding,
+        INDEX_NAME,
+    )
+
+    
